daff3780c5263389a2cb02e311f6876631ca4065
Adame Naji <adamenaji@hotmail.fr>
2019-11-20 09:24:02 +0100

    modification lander.py, ajout d'une image d'un lander à retravailler

--- lander.py
+++ lander.py
@@ -4,8 +4,14 @@ import pygame
 
 # Classes
 class Gameconfig :
+    white = (255, 255, 255)
+
     windowH = 1300
     windowW = 700
+
+    imgLander = pygame.image.load('lander.png')
+    landerH = 50
+    landerW = 50
 
 
 class GameState:
@@ -13,24 +19,29 @@ class GameState:
         self.landerModuleX = 100
         self.landerModuleY = 100
 
+    def draw(self, window):
+        window.blit(Gameconfig.imgLander, (self.landerModuleX, self.landerModuleY))
 
 # Fonctions
 
 
-def gameLoop() : #Boucle de jeu
+def gameLoop(window) : #Boucle de jeu
     game_over = False
+    gameState = GameState()
 
     while not game_over:
+        gameState.draw(window)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
-
+        pygame.display.update()
 
 def main(): #Fonction principale
     pygame.init()
     window = pygame.display.set_mode((Gameconfig.windowH, Gameconfig.windowW))
+    pygame.display.set_caption("Planet Lander")
 
-    gameLoop()
+    gameLoop(window)
     pygame.quit()
     quit()
 
