ef9c9908e8ba9453f06c7402ad0198cff99025ae
mxb <mxb@mxb.com>
2019-11-21 20:04:05 +0100

    Moved config to fix circular issue.

---
+++ GameConfig.py
@@ -0,0 +1,15 @@
+# Définition des classes
+import pygame
+
+
+class GameConfig:
+    # Taille de la fenêtre
+    windowW = 1500
+    windowH = 700
+
+    # Initlisation du lander et de sa taille
+    lander = pygame.image.load('lander_normal.png')
+    imgLanderW = 30
+    imgLanderH = 30
+    white = (255, 255, 255)
+    red = (255, 0, 0)
