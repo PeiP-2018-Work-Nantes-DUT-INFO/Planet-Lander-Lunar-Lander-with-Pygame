02c848f334417f1a941563089fc49d6dfdff1d9d
Adame Naji <adamenaji@hotmail.fr>
2019-11-25 15:50:29 +0100

    update Vector Class

--- Vecteur.py
+++ Vecteur.py
@@ -5,21 +5,22 @@ import pygame
 
 
 class Vecteur:
-    def __init__(self, x, y, angle, norme=None):
+    def __init__(self, x, y, angle=None, norme=None):
+        # Constructeurs
         self.x = x
         self.y = y
 
-        if angle is not None and norme is not None: #Dans le cas ou le vaisseau est déjà soumis à un vecteur de force.
+        if angle is not None and norme is not None:  # Dans le cas ou le vaisseau est déjà soumis à un vecteur de force.
             self.x = norme + math.sin(math.radians(angle))
             self.y = norme + math.cos(math.radians(angle))
 
-        @property
+        @property  # Permet de définir la norme
         def norme(self):
             return math.sqrt(self.x**2 + self.y**2)
 
-        @property
+        @property  # Permet de définir l'angle
         def angle(self):
-            if math.floor(self.y == 0): #On met le Math.floor uniquement ici pour éviter d'avoir une norme en (0,0)
+            if math.floor(self.y == 0):  # On met le Math.floor uniquement ici pour éviter d'avoir une norme en (0,0)
                 if self.x > 0:
                     return 90.0
                 else:
@@ -31,3 +32,11 @@ class Vecteur:
                     return 0.0
             else:
                 return math.degrees(math.tan(self.x/self.y))
+
+        # Méthodes de classe
+        def __add__(self, newVecteur):
+            return Vecteur(x=(self.x + newVecteur.x), y=(self.y + newVecteur.y))
+
+        def __str__(self):
+            return "X: %.3d Y: %.3d Angle: %.3d degrees Norme: %.3d" % (self.x, self.y, self.angle, self.norme)
+
