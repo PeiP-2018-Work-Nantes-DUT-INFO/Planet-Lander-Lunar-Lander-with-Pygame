691cf697b979f5df4b2818d57a1c7970b900f3bf
Adame Naji <adamenaji@hotmail.fr>
2019-11-28 20:14:46 +0100

    Add file for IA

--- components/artificial_intelligence_1.py
+++ components/artificial_intelligence_1.py
@@ -1,4 +1,5 @@
 import pygame
+from os import path
 
 
 class Artificial_intelligence():
@@ -9,79 +10,89 @@ class Artificial_intelligence():
                 self.plateform_arrivee = plateform
                 value_max = plateform[3]
         if plateform[3] == 1:
-            self.file = open('distance_1.txt', 'w+')
+            self.file = open(path.join('ressources', 'distance_1.txt'), 'r')
         elif plateform[3] == 2:
-            self.file = open('distance_2.txt', 'w+')
+            self.file = open(path.join('ressources', 'distance_2.txt'), 'r')
         else:
-            self.file = open('distance_4.txt', 'w+')
+            self.file = open(path.join('ressources', 'distance_4.txt'), 'r')
         self.distance = float(self.file.read())
 
         self.aircraft = aircraft
 
     def get_next_commande(self):
+        print(self.aircraft.orientation)
+        self.aircraft.orientation %= 360
         if self.aircraft.x < (self.plateform_arrivee[0] + (self.plateform_arrivee[2] / 2) - self.distance): # On est à gauche de la plateforme
+            print("gauche")
             if self.aircraft.vx > 25:
-                if self.aircraft.orientation != 0:
-                    return self.turn_0()
+                if self.aircraft.orientation != 270:
+                    return self.turn_270()
                 if self.aircraft.y > 140 and self.aircraft.vy > -10:
                     return pygame.K_UP
             if self.aircraft.vx < 25:
-                if self.aircraft.orientation != 270.0:
-                    return self.turn_270()
+                if self.aircraft.orientation != 0:
+                    return self.turn_0()
                 return pygame.K_UP
 
-        if self.aircraft.x < (self.plateform_arrivee[0] + (self.plateform_arrivee[2] / 2) + self.distance): # On est à droite de la plateforme
-            if self.aircraft.vx < 25:
-                if self.aircraft.orientation != 0:
-                    return self.turn_0()
+        if self.aircraft.x > (self.plateform_arrivee[0] + (self.plateform_arrivee[2] / 2) + self.distance): # On est à droite de la plateforme
+            print("droite")
+            if self.aircraft.vx > 25:
+                if self.aircraft.orientation != 270:
+                    return self.turn_270()
                 if self.aircraft.y > 140 and self.aircraft.vy > -10:
                     return pygame.K_UP
-            if self.aircraft.vx > 25:
-                if self.aircraft.orientation != 270.0:
-                    return self.turn_270()
+            if self.aircraft.vx < 25:
+                if self.aircraft.orientation != 180.0:
+
+                    return self.turn_180()
                 return pygame.K_UP
 
         if (self.plateform_arrivee[0] + (self.plateform_arrivee[2] / 2) - self.distance) < self.aircraft.x < (self.plateform_arrivee[0] + (self.plateform_arrivee[2] / 2)):
+            print("in_gauche")
+            if self.aircraft.vx > 0.0:
+                if self.aircraft.orientation != 180:
+                    return self.turn_180()
+                if self.aircraft.vx > 0:
+                    return pygame.K_UP
+
             if self.aircraft.orientation != 270:
                 return self.turn_270()
-            if self.aircraft.vx > 0:
+            if self.aircraft.vy > 15:
                 return pygame.K_UP
-
-            if self.aircraft.vx == 0:
-                if self.aircraft.orientation != 0:
-                    return self.turn_0()
-                if self.aircraft.vy > 15:
-                    return pygame.K_UP
 
         if (self.plateform_arrivee[0] + (self.plateform_arrivee[2] / 2)) < self.aircraft.x < (self.plateform_arrivee[0] + (self.plateform_arrivee[2] / 2) + self.distance):
-            if self.aircraft.orientation != 90:
-                return self.turn_90()
+            print("in_droite")
+            if self.aircraft.orientation != 0:
+                return self.turn_0()
             if self.aircraft.vx > 0:
                 return pygame.K_UP
 
             if self.aircraft.vx == 0:
-                if self.aircraft.orientation != 0:
-                    return self.turn_0()
+                if self.aircraft.orientation != 270:
+                    return self.turn_270()
                 if self.aircraft.vy > 15:
                     return pygame.K_UP
 
-    def turn_270(self):
-        if self.aircraft.orientation > 180.0:
-            return pygame.K_RIGHT
-        else:
-            return pygame.K_LEFT
-
-    def turn_90(self):
-        if self.aircraft.orientation > 180.0:
-            return pygame.K_RIGHT
-        else:
-            return pygame.K_LEFT
-
     def turn_0(self):
+        print("turn_0")
         if self.aircraft.orientation > 180.0:
             return pygame.K_RIGHT
         else:
             return pygame.K_LEFT
+
+    def turn_270(self):
+        print("turn_270")
+        if 90.0 < self.aircraft.orientation < 270.0:
+            return pygame.K_RIGHT
+        else:
+            return pygame.K_LEFT
+
+    def turn_180(self):
+        print("turn_180")
+        if self.aircraft.orientation > 180.0:
+            return pygame.K_LEFT
+        else:
+            return pygame.K_RIGHT
 
     def update_distance(self):
         if self.aircraft.x > self.plateform_arrivee[0] + (self.plateform_arrivee[2] / 2):
